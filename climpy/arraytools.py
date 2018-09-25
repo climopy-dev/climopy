6418916bb6f953613c01d125461cc971c1cb7789
Luke Davis <lukelbd@gmail.com>
2018-09-25 14:40:17 -0600

    Fix weird merge bs

--- climpy/arraytools.py
+++ climpy/arraytools.py
@@ -3,54 +3,92 @@
 Includes several helper/base functions shared by other tools.
 """
 import numpy as np
-def trail_flatten(data, nflat):
-    """
-    Flatten *trailing* dimensions onto single dimension (useful).
-    Call this to manipulate data on first dimensions, then restore with unflatten.
-    * Note that numpy prod of an empty iterable will be 1; so, adds singleton dim.
-    * Note we use tuple expansion of the [shape] tuple
-    * Note default for numpy is is row-major.
-    """
-    return np.reshape(data, (*data.shape[:-nflat], np.prod(data.shape[-nflat:]).astype(int)), order='F'), data.shape
 
-def trail_unflatten(data, shape, nflat):
+def trail_flatten(data, nflat=None):
     """
-    Undo action of flatten.
-    Shape can be the original shape, or a new shape.
+    Flatten *trailing* dimensions onto single dimension.
+    Argument 'nflat' controls number of dimensions flattened; default is to
+    choose <ndim-1> i.e. enough to flatten into a 2x2 array.
+     * Note that numpy prod of an empty iterable will be 1; so, adds singleton dim.
+     * Note we use tuple expansion of the [shape] tuple
+     * Note default for numpy is is row-major.
     """
+    shape = list(data.shape)
+    if nflat is None:
+        nflat = data.ndim-1 # all but last dimension
+    if nflat<=1: # just apply singleton dimension
+        return data[...,None], [*shape, 1]
+    if nflat is None:
+        nflat = data.ndim-1 # all but last dimension
+    return np.reshape(data, (*data.shape[:-nflat], np.prod(data.shape[-nflat:]).astype(int)), order='F'), shape
+
+def trail_unflatten(data, shape, nflat=None):
+    """
+    Undo action of trail_flatten.
+    Argument 'shape' is desired shape of reconstructed array.
+    Argument 'nflat' controls number of dimensions unflattened; default is to
+    choose <ndim-1> i.e. enough to flatten into a 2x2 array.
+    """
+    if nflat is None:
+        nflat = len(shape)-1 # all but last dimension
+    if nflat<=1: # just remove singleton dimension
+        return data[...,0]
     if data.shape[-1] != np.prod(shape[-nflat:]):
         raise ValueError(f'Number of trailing elements {data.shape[-1]} does not match trailing shape {shape[nflat:]:s}.')
     if not all(s1==s2 for s1,s2 in zip(data.shape[:-1], shape[:-nflat])):
         raise ValueError(f'Leading dimensions on data, {data.shape[:-1]}, do not match leading dimensions on new shape, {shape[:-nflat]}.')
     return np.reshape(data, shape, order='F')
 
-def lead_flatten(data, nflat):
+def lead_flatten(data, nflat=None):
     """
     Flatten *leading* dimensions onto single dimension.
+    Argument 'nflat' controls number of dimensions flattened; default is to
+    choose <ndim-1> i.e. enough to flatten into a 2x2 array.
     """
-    return np.reshape(data, (np.prod(data.shape[:nflat]).astype(int), *data.shape[nflat:]), order='C') # make column major
+    shape = list(data.shape)
+    if nflat is None:
+        nflat = data.ndim-1 # all but last dimension
+    if nflat<=1: # just apply singleton dimension
+        return data[None,...], [1, *shape]
+    # if np.sum(data.shape[:nflat])==0:
+    #     shape = [1, *shape]
+    return np.reshape(data, (np.prod(data.shape[:nflat]).astype(int), *data.shape[nflat:]), order='C'), shape # make column major
 
-def lead_unflatten(data, shape, nflat):
+def lead_unflatten(data, shape, nflat=None):
     """
-    Undo action of leadflatten.
+    Undo action of lead_flatten.
+    Argument 'shape' is desired shape of reconstructed array.
+    Argument 'nflat' controls number of dimensions unflattened; default is to
+    choose <ndim-1> i.e. enough to flatten into a 2x2 array.
     """
+    if nflat is None:
+        nflat = len(shape)-1 # all but last dimension
+    if nflat<=1: # just remove singleton dimension
+        return data[0,...]
     if data.shape[0] != np.prod(shape[:nflat]):
-        raise ValueError(f'Number of leading elements {data.shape[0]} does not match leading shape {shape[end:]:s}.')
+        raise ValueError(f'Number of leading elements {data.shape[0]} does not match leading shape {shape[nflat:]}.')
     if not all(s1==s2 for s1,s2 in zip(data.shape[1:], shape[nflat:])):
         raise ValueError(f'Trailing dimensions on data, {data.shape[1:]}, do not match tailing dimensions on new shape, {shape[nflat:]}.')
     return np.reshape(data, shape, order='F')
 
-def permute(data, axis=-1):
+def permute(data, axis=-1, select=-1):
     """
-    Permutes a given axis onto the LAST dimension.
+    Permutes axis <axis> onto the *last* dimension.
+    Rollaxis moves axis until to *left* of axis number data.ndim, i.e. to
+    the end because that is 1 more than the last axis number.
     """
-    return np.rollaxis(data, axis, data.ndim)
+    if select<0:
+        select = data.ndim + select
+    return np.rollaxis(data, axis=axis, start=select)
 
 def unpermute(data, axis=-1, select=-1):
     """
-    Undoes action of permute; permutes LAST dimension back onto original axis.
-    Rolls axis <select> until it lies "before" (i.e. to the left) of axis <axis>.
+    Undoes action of permute; permutes *last* dimension back onto axis <axis>.
+    Alternatively, roll the different axis <select> until it lies "before"
+    (i.e. to the left) axis <axis>, which is useful e.g. if you added some new
+    trailing dimensions.
     """
-    if select<0: select = data.ndim+select
-    return np.rollaxis(data, select, axis)
+    if select<0:
+        select = data.ndim + select
+    return np.rollaxis(data, axis=select, start=axis)
 
