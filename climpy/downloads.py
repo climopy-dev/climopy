8bc88d3e77becd395c4b941a6e84eb46220772e9
Luke Davis <lukelbd@gmail.com>
2019-01-29 19:37:12 -0700

    Update downloader

--- climpy/downloads.py
+++ climpy/downloads.py
@@ -2,6 +2,7 @@
 # Imports
 import xarray as xr
 import numpy as np
+import calendar
 
 #-------------------------------------------------------------------------------
 # ERA-interim downloads
@@ -9,6 +10,8 @@ import numpy as np
 def eraint(params, stream, levtype,
         daterange=None, yearrange=None, monthrange=None, dayrange=None,
         years=None, months=None, # can specify list
+        format='netcdf', # file format
+        forecast=False, step=12, # whether we want the *forecast* field? and forecast timestep if so
         levrange=None, levs=None,
         hours=(0,6,12,18), hour=None,
         res=1.0, box=None,
@@ -39,10 +42,14 @@ def eraint(params, stream, levtype,
         individual variable string
     *** Must know MARS id for requested params; can add to dictionary in code below using
         https://rda.ucar.edu/datasets/ds627.0/docs/era_interim_grib_table.html
+        Pay attention to *available groups*. If not available for the group
+        you selected (e.g. pressure levs, moda), get ERROR 6 (MARS_EXPECTED_FIELDS).
+    *** For rates of change of *parameterized* processes (i.e. diabatic) see:
+        https://confluence.ecmwf.int/pages/viewpage.action?pageId=57448466
     stream: can be any of:
-        'synoptic' (6 hourly DATA)
-        'monthly' (monthly mean)
+        'oper', 'moda', 'mofm', 'mdfa', or 'mnth'
     levtype: can be any of:
+        'ml' (model levels)
         'pl' (pressure levels)
         'sfc' (earth surface)
         'pt' (potential temperature)
@@ -56,27 +63,38 @@ def eraint(params, stream, levtype,
     hours: can be either of:
         list/tuple of integer hours (should be in [0,6,12,18])
         single number, to pick individual hour
+    forecast:
+        whether we want forecast type ('fc') or analysis type ('an')
     res: desired output resolution; not sure if this is arbitrary, or if ERA-interim only has
         a select few valid resolution options.
     box: can be either of:
         string name for particular region, e.g. "europe" (see documentation)
         the West/South/East/North boundaries (so lower-left corner, upper-right corner), as a length-4 list/tuple
+    format:
+        one of 'grib1', 'grib2', or 'netcdf'
     filename:
         name of file output
+
+    Notes
+    -----
+    Some fields (seems true for most model fields) are not archived as
+    monthly means for some reason! Have no idea why because it would need
+    almost zero storage requirements.
     """
     # Data stream
     import ecmwfapi as ecmwf # only do so inside function
-    stream = { # oper is original, moda is monthly mean of daily means
-            'synoptic':  'oper',
-            'monthly':   'moda'
-            }.get(stream)
-    if stream is None:
-        raise ValueError('Must choose from "oper" for synoptic fields, "moda" for monthly means of daily means.')
+    # stream = { # oper is original, moda is monthly mean of daily means
+    #         'synoptic':  'oper',
+    #         'monthly':   'moda'
+    #         }.get(stream)
+    # if stream is None:
+    #     raise ValueError('Must choose from "oper" for synoptic fields, "moda" for monthly means of daily means.')
 
     # Variable id conversion (see: https://rda.ucar.edu/datasets/ds627.0/docs/era_interim_grib_table.html)
-    if isinstance(params, str):
+    if isinstance(params, str) or not np.iterable(params):
         params = (params,)
     params = [{
+            'tdt':     '110.162',
             't2m':     '167.128', # 2m temp
             'd2m':     '168.128', # 2m dew point
             'sst':     '34.128', # sst
@@ -106,11 +124,10 @@ def eraint(params, stream, levtype,
     # Time selection as various RANGES or LISTS
     # Priority; just use daterange as datetime or date objects
     if daterange is not None:
-        try: iter(daterange)
-        except TypeError:
+        if not np.iterable(daterange):
             daterange = (daterange,) # want a SINGLE DAY
         # options for monthly or daily data
-        if stream=='moda':
+        if stream!='oper':
             y0, m0, y1, m1 = daterange[0].year, daterange[0].month, daterange[1].year, daterange[1].month
             N = max(y1-y0-1, 0)*12 + (13-m0) + m1 # number of months in range
             dates = '/'.join('%04d%02d00' % (y0 + (m0+n-1)//12, (m0+n-1)%12 + 1) for n in range(N))
@@ -121,12 +138,10 @@ def eraint(params, stream, levtype,
     else:
         # First, years
         if years is not None:
-            try: iter(years)
-            except TypeError:
+            if not np.iterable(years):
                 years = (years,) # single month
         elif yearrange is not None:
-            try: iter(yearrange)
-            except TypeError: # single year
+            if not np.iterable(yearrange):
                 years = (yearrange,)
             else:
                 years = tuple(range(yearrange[0], yearrange[1]+1))
@@ -134,19 +149,17 @@ def eraint(params, stream, levtype,
             raise ValueError('You must use "years" or "yearrange" kwargs.')
         # Next, months (this way, can just download JJA data, for example)
         if months is not None:
-            try: iter(months)
-            except TypeError:
+            if not np.iterable(months):
                 months = (months,) # single month
         elif monthrange is not None:
-            try: iter(monthrange)
-            except TypeError: # single year
+            if not np.iterable(monthrange):
                 months = (monthrange, monthrange)
             else:
                 months = tuple(range(monthrange[0], monthrange[1]+1))
         else:
             months = tuple(range(1,13))
         # And get dates; options for monthly means and daily stuff
-        if stream=='moda':
+        if stream!='oper':
             dates = '/'.join(
                 '/'.join('%04d%02d00' % (y,m) for m in months)
                 for y in years)
@@ -162,23 +175,23 @@ def eraint(params, stream, levtype,
     levchoices = {
             'sfc':  None,
             'pv':   None,
-            'pl':   np.array([1,2,3,5,7,10,20,30,50,70,100,125,150,175,200,225,250,300,350,400,450,500,550,600,650,700,750,775,800,825,850,875,900,925,950,975,1000]),
-            'pt':   np.array([265,270,285,300,315,330,350,370,395,430,475,530,600,700,850]),
+            'ml': np.arange(1,137+1),
+            'pl': np.array([1,2,3,5,7,10,20,30,50,70,100,125,150,175,200,225,250,300,350,400,450,500,550,600,650,700,750,775,800,825,850,875,900,925,950,975,1000]),
+            'pt': np.array([265,270,285,300,315,330,350,370,395,430,475,530,600,700,850]),
             }.get(levtype, [])
     if levchoices==[]:
         raise ValueError('Invalid level type. Choose from "pl", "pt", "pv", "sfc".')
+    # More options
     if levtype not in ('sfc','pv'): # these have multiple options
-        # require input
+        # Require input
         if levs is None and levrange is None and levtype not in ('sfc','pv'):
             raise ValueError('Must specify list of levels to "levs" kwarg, range of levels to "levrange" kwarg, or single level to either one.')
-        # convert levels to mars request
+        # Convert levels to mars request
         if levs is not None:
-            try: iter(levs)
-            except TypeError: # single level
+            if not np.iterable(levs):
                 levs = (levs,)
         elif levrange is not None:
-            try: iter(levrange)
-            except TypeError: # single level
+            if not np.iterable(levrange):
                 levs = (levrange,)
             else:
                 levs = levchoices[(levchoices>=levrange[0]) & (levchoices<=levrange[1])].flat
@@ -191,10 +204,16 @@ def eraint(params, stream, levtype,
     if box is not None and type(box) is not str:
         box = '/'.join(str(b) for b in (box[3], box[0], box[2], box[1]))
     # Hour conversion
-    try: iter(hours)
-    except TypeError:
+    if not np.iterable(hours):
         hours = (hours,)
     hours = '/'.join(str(h).zfill(2) for h in hours) # zfill padds 0s on left
+    # Forecast type
+    # TODO: Fix the 12 hour thing. Works for some params (e.g. diabatic
+    # heating, has 3, 6, 9, 12) but others have 0, 6, 12, 18.
+    if forecast:
+        dtype, step = 'fc', str(step)
+    else:
+        dtype, step = 'an', '0'
 
     # Server instructions
     # Not really sure what happens in some situations: list so far:
@@ -202,22 +221,23 @@ def eraint(params, stream, levtype,
     #       MARS will search for correct one; if there is name ambiguity/conflict will throw error
     # 2) on GUI framework, ECMWF only offers a few resolution options, but program seems
     #       to run when requesting custom resolutions like 5deg/5deg
-    retrieve = {
+    request = {
+        # Important ones
         'class':    'ei', # ecmwf classifiction; choose ERA-Interim
         'expver':   '1',
         'dataset':  'interim', # thought we already did that; *shrug*
-        'type':     'an', # type of field; analysis 'an' or forecast 'fc'
+        'type':     dtype, # type of field; analysis 'an' or forecast 'fc'
         'resol':    'av', # prevents truncation before transformation to geo grid
-        'step':     '0', # number of hours forecast has been run into future from 'time'
         'gaussian': 'reduced',
-        # 'format':   'netcdf',
+        'format':   format,
+        'step':     step, # NOTE: ignored for non-forecast type
         # 'grid':     res,
-        'format':   'grib', # can also spit raw output into GRIB; apparently
-            # ERA-Interim uses bilinear interpolation to make grid of point obs,
-            # which makes sense, because their reanalysis model just picks out point observations
-            # from spherical harmonics; so maybe grid cell concept is dumb? maybe need to focus
-            # on just using cosine weightings, forget about rest?
-        # 'grid':     'N32',
+        # Can also spit raw output into GRIB; apparently ERA-Interim uses
+        # bilinear interpolation to make grid of point obs, which makes sense,
+        # because their reanalysis model just picks out point observations
+        # from spherical harmonics; so maybe grid cell concept is dumb? maybe need to focus
+        # on just using cosine weightings, forget about rest?
+        'grid':     'N32',
         'stream':   stream, # product monthly, raw, etc.
         'date':     dates,
         'time':     hours,
@@ -225,12 +245,16 @@ def eraint(params, stream, levtype,
         'param':    params,
         'target':   filename, # save location
         }
-    if levs is not None: retrieve.update(levelist=levs)
-    if box is not None: retrieve.update(area=box)
-    if stream!='moda': retrieve.update(hour=hour)
-    print('Final MARS request: %s' % retrieve)
+    maxlen = max(map(len, request.keys()))
+    print("REQUEST\n" + "\n".join(f'"{key}": ' + " "*(maxlen - len(key)) + f"{value}" for key,value in request.items()))
+    if levs is not None:
+        request.update(levelist=levs)
+    # if box is not None:
+    #     request.update(area=box)
+    if stream!='moda':
+        request.update(hour=hour)
     # Retrieve DATA with settings
     server = ecmwf.ECMWFDataServer()
-    server.retrieve(retrieve)
+    server.retrieve(request)
     return
 
