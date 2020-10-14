a6315d8374b8a60cce7dac0f0b953642e79d9b9e
Luke Davis <lukelbd@gmail.com>
2020-10-14 03:30:04 -0600

    Cleanup constants code

--- climopy/const.py
+++ climopy/const.py
@@ -11,8 +11,6 @@ import pint
 
 from .units import ureg
 
-Quant = ureg.Quantity
-
 __all__ = [
     'G', 'H', 'Md', 'Mw', 'Na', 'Omega', 'R',
     'a', 'c', 'cp', 'cv', 'e', 'g', 'h', 'p0', 'psfc', 'pi', 'tau',
@@ -22,72 +20,72 @@ __all__ = [
 
 #: `Gravitational constant\
 #: <https://en.wikipedia.org/wiki/Gravitational_constant>`__
-G = Quant(6.67408e-11, 'm^3 kg^-1 s^-2')
+G = ureg.Quantity(6.67408e-11, 'm^3 kg^-1 s^-2')
 
 #: Rule-of-thumb 7km atmospheric
 #: `scale height <http://glossary.ametsoc.org/wiki/Scale_height>`__
-H = Quant(7.0e3, 'm')
+H = ureg.Quantity(7.0e3, 'm')
 
 #: Dry air molar mass
-Md = Quant(28.9645e-3, 'kg mol^-1')
+Md = ureg.Quantity(28.9645e-3, 'kg mol^-1')
 
 #: Water vapor molar mass
-Mw = Quant(18.0153e-3, 'kg mol^-1')
+Mw = ureg.Quantity(18.0153e-3, 'kg mol^-1')
 
 #: `Avogadro constant\
 #: <https://en.wikipedia.org/wiki/Avogadro_constant>`__.
-Na = Quant(6.02214e23, 'mol^-1')
+Na = ureg.Quantity(6.02214e23, 'mol^-1')
 
 #: Earth `rotation rate\
 #: <https://en.wikipedia.org/wiki/Earth%27s_rotation#Angular_speed>`__
-Omega = Quant(7.292115e-5, 'rad s^-1')
+Omega = ureg.Quantity(7.292115e-5, 'rad s^-1')
 
 #: `Ideal gas constant\
 #: <https://en.wikipedia.org/wiki/Gas_constant>`__
-R = Quant(8.31446, 'J mol^-1 K^-1')
+R = ureg.Quantity(8.31446, 'J mol^-1 K^-1')
 
 #: Earth `mean radius\
 #: <https://en.wikipedia.org/wiki/Earth_radius#Mean_radius>`__
-a = Quant(6.3710088e6, 'm')
+a = ureg.Quantity(6.3710088e6, 'm')
 
 #: `Speed of light\
 #: <http://glossary.ametsoc.org/wiki/Speed_of_light>`__ in a vacuum
-c = Quant(2.99792458e8, 'm s^-1')
+c = ureg.Quantity(2.99792458e8, 'm s^-1')
 
 #: `Specific heat capacity\
 #: <http://glossary.ametsoc.org/wiki/Specific_heat_capacity>`__
 #: at constant pressure for dry air at :math:`0^{\circ}\mathrm{C}`
-cp = Quant(1.0057e3, 'J kg^-1 K^-1')
+cp = ureg.Quantity(1.0057e3, 'J kg^-1 K^-1')
 
 #: `Specific heat capacity\
 #: <http://glossary.ametsoc.org/wiki/Specific_heat_capacity>`__
 #: at constant volume for dry air at :math:`0^{\circ}\mathrm{C}`
-cv = Quant(0.719e3, 'J kg^-1 K^-1')
+cv = ureg.Quantity(0.719e3, 'J kg^-1 K^-1')
 
 #: `Euler's number\
 #: <https://en.wikipedia.org/wiki/E_(mathematical_constant)>`__
-e = math.e
+e = ureg.Quantity(math.e, '')
 
 #: `Standard acceleration due to gravity\
 #: <https://en.wikipedia.org/wiki/Standard_gravity>`__
-g = Quant(9.80665, 'm s^-2')
+g = ureg.Quantity(9.80665, 'm s^-2')
 
 #: `Planck constant\
 #: <http://glossary.ametsoc.org/wiki/Planck%27s_constant>`__
-h = Quant(6.62607e-34, 'J s')
+h = ureg.Quantity(6.62607e-34, 'J s')
 
 #: Earth `mean sea-level pressure\
 #: <https://en.wikipedia.org/wiki/Atmospheric_pressure#Mean_sea-level_pressure>`__
-psfc = Quant(101325.0, 'Pa')
+psfc = ureg.Quantity(101325.0, 'Pa')
 
 #: Standard reference pressure
-p0 = Quant(1e5, 'Pa')
+p0 = ureg.Quantity(1e5, 'Pa')
 
 #: :math:`\pi` (3.14159...)
-pi = math.pi
+pi = ureg.Quantity(math.pi, '')
 
 #: :math:`\tau` (6.28318...)
-tau = 2.0 * pi
+tau = ureg.Quantity(2.0 * pi, '')
 
 #: Dry air gas constant
 Rd = R / Md
@@ -123,15 +121,18 @@ def _add_transformation(context, source, dest, scale):  # noqa: E302
     Add linear forward and inverse unit transformations.
     """
     context.add_transformation(
-        source, dest, functools.partial(lambda scale, ureg, x: x * scale, scale)
+        source, dest, functools.partial(lambda scale, _, x: x * scale, scale)
     )
     context.add_transformation(
-        dest, source, functools.partial(lambda scale, ureg, x: x / scale, scale)
+        dest, source, functools.partial(lambda scale, _, x: x / scale, scale)
     )
 
 
 # Static energy components, their rates of change (1/s), their fluxes (m/s), and
 # their *absolute* fluxes integrated over the latitude band (m^2/s).
+# NOTE: Pint context transformations are recursive (e.g. below permits converting
+# [length] to [temperature]) but not multiplicative (e.g. below does not cover
+# converting [temperature] * [mass] to [energy]).
 # NOTE: Common to want to convert [energy] / [mass] to [energy] / [area] / [pressure]
 # for displaying static or Lorenz energy terms. But this is already covered by
 # the geopotential height transformations! Latter units are equivalent to [length]!
@@ -154,5 +155,5 @@ for suffix1, suffix2 in itertools.product(
         cp,
     )
 
-# Add context object
+# Register context object
 ureg.add_context(climo)
