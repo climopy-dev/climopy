56c969c10d10dc21b1a39cf538403e9d07507061
Luke Davis <lukelbd@gmail.com>
2021-01-07 03:02:54 -0700

    Update __init__

--- climopy/__init__.py
+++ climopy/__init__.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 """
-Instantiate the `pint.UnitRegistry` unit registry.
+A python package for climate scientists.
 """
-# Import functions to top-level
-# NOTE: Syntax is 'from climopy import ureg, const'
+# Import functions to top-level. Recommended syntax for registries is one of:
+# from climopy import const, units, descriptors
+# from climopy import const, ureg, dreg
+# NOTE: Submodules are for organization and should not be accessible by users
 import pkg_resources as _pkg
-from .units import *  # noqa: F401, F403
+from .unit import *  # noqa: F401, F403
+from .cfvariable import *  # noqa: F401 F403
+from .accessor import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
-from .spherical import *  # noqa: F401, F403
-from .downloads import *  # noqa: F401, F403
 from .diff import *  # noqa: F401, F403
 from .var import *  # noqa: F401, F403
-from .spectral import *  # noqa: F401, F403
 from .waves import *  # noqa: F401, F403
+from .spectral import *  # noqa: F401, F403
+from .spherical import *  # noqa: F401, F403
+from .downloads import *  # noqa: F401, F403
 from . import const  # noqa: F401
 from . import internals  # noqa: F401
 
